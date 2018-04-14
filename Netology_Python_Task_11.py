329c466cc4ffabe8f1ca0c155c8a2ca9fa01f879
Marat Genatulin <marat@genatulin.com>
2018-04-14 17:28:43 +0300

    Работа над ошибками - исправление замечаний преподавателя.

--- Netology_Python_Task_11.py
+++ Netology_Python_Task_11.py
@@ -1,11 +1,11 @@
 """Программа по подсчету частоты использования слов в новостных файлах."""
 
 
+import json
 import operator
 
 import chardet
 
-import json
 
 class Newsfile(object):
     """Класс - новостной файл."""
@@ -20,7 +20,7 @@ class Newsfile(object):
         if file_format == 'txt':
             self.news = self.open_file_txt(filename).decode(self.encoding)
         elif file_format == 'json':
-            self.convert_json_txt(self.open_file_json(filename, code_file))
+            self.news = self.open_file_json(filename, code_file)
         self.name = filename
 
     def open_file_txt(self, filename):
@@ -38,26 +38,27 @@ class Newsfile(object):
             self.encoding = 'json, ' + code_file
             return news
 
-    def convert_json_txt(self, news):
-        """Конвертирование json в txt."""
-        for keys in news:
-            if type(news[keys]) == type(dict()):
-                self.convert_json_txt(news[keys])
-            elif type(news[keys]) == type(list()):
-                for items in news[keys]:
-                    self.news += items['title'] + ' '
-                    self.news += items['description'] + ' '
-
-    def count_top(self, n=6):
-        """Подсчет количества слов в файле. Определение рейтинга слов."""
-        self.words = {}
-        our_list = list(self.news.split())
+    def countword(self, count_text, n):
+        """Подсчет количества слов в тексте."""
+        our_list = list(count_text.split())
         for item in our_list:
             if len(item.strip()) >= n:
                 if self.words.get(item) is None:
                     self.words[item] = 1
                 else:
                     self.words[item] += 1
+
+    def count_top(self, n=6):
+        """Подсчет количества слов в файле. Определение рейтинга слов."""
+        self.words = {}
+        if self.encoding.find('json') != -1:
+            news = self.news['rss']['channel']['items']
+            for piece_of_news in news:
+                self.countword(piece_of_news['title'], n)
+                self.countword(piece_of_news['description'], n)
+        else:
+            self.countword(self.news, n)
+
         self.words = sorted(self.words.items(),
                             key=operator.itemgetter(1), reverse=True)
 
@@ -72,11 +73,17 @@ class Newsfile(object):
                 break
         print('----------')
 
+
 def work_with_file(files, type_of_file, n, top, code_page):
-    file_1 = Newsfile('' + files[0] + '.' + type_of_file, type_of_file, code_page[0])
-    file_2 = Newsfile(files[1] + '.' + type_of_file, type_of_file, code_page[1])
-    file_3 = Newsfile(files[2] + '.' + type_of_file, type_of_file, code_page[2])
-    file_4 = Newsfile(files[3] + '.' + type_of_file, type_of_file, code_page[3])
+    """Выполнение операций над файлами."""
+    file_1 = Newsfile('' + files[0] + '.'
+                      + type_of_file, type_of_file, code_page[0])
+    file_2 = Newsfile(files[1] + '.'
+                      + type_of_file, type_of_file, code_page[1])
+    file_3 = Newsfile(files[2] + '.'
+                      + type_of_file, type_of_file, code_page[2])
+    file_4 = Newsfile(files[3] + '.'
+                      + type_of_file, type_of_file, code_page[3])
 
     file_1.count_top(n)
     file_2.count_top(n)
@@ -98,10 +105,12 @@ def main():
     while n <= 0:
         n = int(input('Длина слова для анализа (не менее символов):'))
 
-    # # Файлы формата txt.
+    # Файлы формата txt.
     work_with_file(files, 'txt', n, top, [None, None, None, None])
 
-    # # Файлы формата json.
-    work_with_file(files, 'json', n, top, ['utf-8', 'KOI8-R', 'ISO-8859-5', 'windows-1251'])
+    # Файлы формата json.
+    work_with_file(files, 'json', n, top,
+                   ['utf-8', 'KOI8-R', 'ISO-8859-5', 'windows-1251'])
+
 
 main()
